1843568d735bb6d925c5a0085953f93767b6281a
kevinfcc26 <kevinfcc26@gmail.com>
2024-06-02 18:36:09 -0500

    feat: load setting variables from environment

--- app/settings.py
+++ app/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sj+v#9vk2@!+!8wk!iv7emhkdjg1ss-r54t6_lvwk+$@g-52(z"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(filter(None, os.environ.get("ALLOWED_HOST", "").split(",")))
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "core",
     "user",
+    "pokemon",
 ]
 
 MIDDLEWARE = [
