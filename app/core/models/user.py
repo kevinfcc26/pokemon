3af4ab6cfa17c6ddaf42120971794d8211e80979
kevinfcc26 <kevinfcc26@gmail.com>
2024-06-01 19:19:55 -0500

    feat: create core app and user model

---
+++ app/core/models/user.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+from app.core.managers.user_manager import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that suppors using email instead od username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
